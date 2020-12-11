e5b1a1eeddc63c505eaf70ad755a992665c77547
Yin Ding <dingyin@gmail.com>
2020-12-10 16:32:56 -0800

    update LF copyright (#25)

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => (
   <footer className="footer background--grey">
     <div className="container">
-      <div className="copyright">© 2020 Centaurus</div>
+      <div className="copyright">Copyright © 2020 Centaurus Infrastructure Project a Series of LF Projects, LLC. Centaurus Infrastructure Project is a trademark of LF Projects, LLC. For trademark usage guidelines, privacy and cookie policies, and other applicable policies, as well as terms and conditions governing this web site, please see www.lfprojects.org.</div>
     </div>
   </footer>
 );
